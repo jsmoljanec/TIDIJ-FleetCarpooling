2616ba469bf2a6f269b7fa843de2d6bcb654a8b0
Domagoj Šantek <dsantek20@foi.hr>
2023-11-10 23:51:07 +0100

    TIDIJ-9 Added radio buttons with value change

--- fleetcarpooling/lib/UserRegistrationForm.dart
+++ fleetcarpooling/lib/UserRegistrationForm.dart
@@ -1,11 +1,20 @@
+import 'package:fleetcarpooling/ui_elements/buttons.dart';
 import 'package:fleetcarpooling/ui_elements/colors';
 import 'package:fleetcarpooling/ui_elements/text_field.dart';
 import 'package:flutter/material.dart';
 
-class UserRegistrationForm extends StatelessWidget {
+enum UserType { Administrator, Employee }
+
+class UserRegistrationForm extends StatefulWidget {
+  @override
+  _UserRegistrationForm createState() => _UserRegistrationForm();
+}
+
+class _UserRegistrationForm extends State<UserRegistrationForm> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController firstNameController = TextEditingController();
   final TextEditingController lastNameController = TextEditingController();
+  UserType _selectedUserType = UserType.Employee;
 
   @override
   Widget build(BuildContext context) {
@@ -72,6 +81,35 @@ class UserRegistrationForm extends StatelessWidget {
               const SizedBox(height: 3.0),
               MyTextField(controller: lastNameController),
               const SizedBox(height: 15.0),
+              const Padding(
+                padding: EdgeInsets.only(left: 24.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Select role:",
+                    style:
+                        TextStyle(color: AppColors.textColor, fontSize: 15.0),
+                  ),
+                ),
+              ),
+              MyRadioButton(
+                title: "Employee",
+                onChanged: (value) {
+                  setState(() {
+                    _selectedUserType = UserType.Employee;
+                  });
+                },
+                value: _selectedUserType == UserType.Employee,
+              ),
+              MyRadioButton(
+                title: "Administrator",
+                onChanged: (value) {
+                  setState(() {
+                    _selectedUserType = UserType.Administrator;
+                  });
+                },
+                value: _selectedUserType == UserType.Administrator,
+              ),
             ],
           ),
         ),
