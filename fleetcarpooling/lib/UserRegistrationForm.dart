56b8eed4fe47acca84eebaa1bd17821961084e9b
Domagoj Šantek <dsantek20@foi.hr>
2023-11-10 23:17:19 +0100

    TIDIJ-9 Added text fields with their labels

--- fleetcarpooling/lib/UserRegistrationForm.dart
+++ fleetcarpooling/lib/UserRegistrationForm.dart
@@ -1,7 +1,12 @@
 import 'package:fleetcarpooling/ui_elements/colors';
+import 'package:fleetcarpooling/ui_elements/text_field.dart';
 import 'package:flutter/material.dart';
 
 class UserRegistrationForm extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +28,53 @@ class UserRegistrationForm extends StatelessWidget {
           ],
         ),
         centerTitle: true,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Padding(
+                  padding: EdgeInsets.only(left: 24.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Email",
+                      style: TextStyle(color: AppColors.textColor),
+                    ),
+                  )),
+              const SizedBox(height: 3.0),
+              MyTextField(controller: emailController),
+              const SizedBox(height: 15.0),
+              const Padding(
+                padding: EdgeInsets.only(left: 24.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "First name",
+                    style: TextStyle(color: AppColors.textColor),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 3.0),
+              MyTextField(controller: firstNameController),
+              const SizedBox(height: 15.0),
+              const Padding(
+                padding: EdgeInsets.only(left: 24.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Last name",
+                    style: TextStyle(color: AppColors.textColor),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 3.0),
+              MyTextField(controller: lastNameController),
+              const SizedBox(height: 15.0),
+            ],
+          ),
+        ),
       ),
     );
   }
