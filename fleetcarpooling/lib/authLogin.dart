3194d46965c6b455cedd5df24313d97960e236af
jsmoljanec <jsmoljane20@student.foi.hr>
2023-11-16 14:09:12 +0100

    TIDIJ-8 Add email lookup functionality to login method

--- fleetcarpooling/lib/authLogin.dart
+++ fleetcarpooling/lib/authLogin.dart
@@ -1,9 +1,39 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class AuthLogin {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Future<String> findEmail(String username) async {
+    final DatabaseReference databaseReference = FirebaseDatabase.instance.ref();
+    var query = await databaseReference
+        .child("Users")
+        .orderByChild('username')
+        .equalTo(username)
+        .limitToFirst(1)
+        .once();
+
+    if (query.snapshot.value != null) {
+      Map<dynamic, dynamic>? userMap =
+          query.snapshot.value as Map<dynamic, dynamic>?;
+
+      if (userMap != null && userMap.isNotEmpty) {
+        String userID = userMap.keys.first;
+        String userEmail = userMap[userID]['email'];
+        return userEmail;
+      }
+    }
+
+    return "";
+  }
+
   Future<void> login({required String email, required String password}) async {
+    if (email.contains('@')) {
+      email = email;
+    } else {
+      email = await findEmail(email);
+    }
+
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: email,
@@ -12,7 +42,6 @@ class AuthLogin {
       print("Login successful: ${userCredential.user?.email}");
     } catch (e) {
       print("Login failed: $e");
-      
     }
   }
 }
