489233e86abcd23776c0bcca636c527801127252
Domagoj Šantek <dsantek20@foi.hr>
2023-11-23 20:54:42 +0100

    TIDIJ-34 Initial screen for adding a vehicle.

---
+++ fleetcarpooling/lib/pages/add_vehicle_form.dart
@@ -0,0 +1,44 @@
+import 'package:fleetcarpooling/ui_elements/buttons.dart';
+import 'package:fleetcarpooling/ui_elements/colors';
+import 'package:flutter/material.dart';
+
+class AddVehicleForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        toolbarHeight: 80,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: AppColors.mainTextColor),
+        title: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(""),
+            Text("ADD NEW CAR",
+                style:
+                    TextStyle(color: AppColors.mainTextColor, fontSize: 25.0)),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 50),
+          MyElevatedButton(onPressed: () {}, label: 'Add vehicle manually'),
+          MyElevatedButton(onPressed: () {}, label: 'Add vehicle with QR code'),
+          Expanded(
+            child: Image.asset(
+              'assets/images/logo.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
