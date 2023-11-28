779b960759351bfa7633b50a34f2c05395b65320
Iva Plavšić <iplavsic20@student.foi.hr>
2023-11-28 10:58:49 +0100

    TIDIJ-28 Designed the top of the notification overview

--- fleetcarpooling/lib/pages/notification_page.dart
+++ fleetcarpooling/lib/pages/notification_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fleetcarpooling/ui_elements/colors';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
@@ -11,8 +12,26 @@ class _NotificationPageState extends State<NotificationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Notification page"),
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    double padding2 = screenHeight * 0.02;
+    return  Scaffold(
+      body:  Column(
+          children: [
+            Container(
+              width: screenWidth,
+              decoration: const BoxDecoration(
+                border: Border(bottom: BorderSide(color: AppColors.buttonColor))
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(top: padding2, bottom: padding2),
+                child: const Text("NOTIFICATIONS",textAlign: TextAlign.center,
+                style: TextStyle(color: AppColors.mainTextColor,fontSize: 24,),
+                ),
+              )
+            )
+          ],
+        ),
     );
   }
 }
