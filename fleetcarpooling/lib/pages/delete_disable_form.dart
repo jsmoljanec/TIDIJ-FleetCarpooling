3d5aff09fcd0749483e5eba3f2dfba0f42d88844
Domagoj Šantek <dsantek20@foi.hr>
2023-11-30 12:01:42 +0100

    TIDIJ-144 Fixed scroll and added padding

--- fleetcarpooling/lib/pages/delete_disable_form.dart
+++ fleetcarpooling/lib/pages/delete_disable_form.dart
@@ -26,14 +26,12 @@ class DeleteDisableForm extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding:
-              const EdgeInsets.only(right: 10, left: 10, top: 20, bottom: 20),
-          child: Container(
-            color: Colors.white,
-            child: VehicleList(),
-          ),
+      body: Padding(
+        padding:
+            const EdgeInsets.only(right: 10, left: 10, top: 20, bottom: 20),
+        child: Container(
+          color: Colors.white,
+          child: VehicleList(),
         ),
       ),
     );
@@ -54,11 +52,15 @@ class VehicleList extends StatelessWidget {
           return Center(child: Text('No vehicles available.'));
         }
 
-        return ListView.builder(
-          itemCount: snapshot.data!.length,
-          itemBuilder: (context, index) {
-            return CardWidget(vehicle: snapshot.data![index]);
-          },
+        return SingleChildScrollView(
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return CardWidget(vehicle: snapshot.data![index]);
+            },
+          ),
         );
       },
     );
@@ -72,31 +74,34 @@ class CardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: AppColors.backgroundColor,
-      child: ListTile(
-        leading: Image.network(vehicle.imageUrl),
-        title: Text(
-          '${vehicle.brand} ${vehicle.model}',
-          style: TextStyle(color: AppColors.mainTextColor),
-        ),
-        subtitle: Text(
-          'Year: ${vehicle.year}',
-          style: TextStyle(color: AppColors.mainTextColor),
-        ),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(
-              icon: Icon(Icons.delete, color: Colors.blue),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(vehicle.active ? Icons.do_not_disturb_on : Icons.add,
-                  color: Colors.blue),
-              onPressed: () {},
-            ),
-          ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child: Card(
+        color: Colors.white,
+        child: ListTile(
+          leading: Image.network(vehicle.imageUrl),
+          title: Text(
+            '${vehicle.brand} ${vehicle.model}',
+            style: TextStyle(color: AppColors.mainTextColor),
+          ),
+          subtitle: Text(
+            '${vehicle.year}',
+            style: TextStyle(color: AppColors.mainTextColor),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                icon: Icon(Icons.delete, color: Colors.blue),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(vehicle.active ? Icons.do_not_disturb_on : Icons.add,
+                    color: Colors.blue),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
