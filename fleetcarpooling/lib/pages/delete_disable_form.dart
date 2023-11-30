37a0e9a3d6bec198ca00733de398d32758c11d05
Domagoj Šantek <dsantek20@foi.hr>
2023-11-30 09:58:17 +0100

    TIDIJ-144 Created cards displaying vehicles

---
+++ fleetcarpooling/lib/pages/delete_disable_form.dart
@@ -0,0 +1,75 @@
+import 'package:fleetcarpooling/Modularity/models/vehicle.dart';
+import 'package:fleetcarpooling/ui_elements/colors';
+import 'package:flutter/material.dart';
+
+class DeleteDisableForm extends StatelessWidget {
+  List<Vehicle> cars = List.empty();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        resizeToAvoidBottomInset: true,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          toolbarHeight: 80,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: AppColors.mainTextColor),
+          title: const Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(""),
+              Text("Delete/disable car",
+                  style: TextStyle(
+                      color: AppColors.mainTextColor, fontSize: 25.0)),
+            ],
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          color: Colors.white,
+          child: VehicleList(),
+        ),
+      ),
+    );
+  }
+}
+
+class VehicleList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<Vehicle>>(
+      stream: null,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(child: Text('No vehicles available.'));
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, index) {
+            return CardWidget(vehicle: snapshot.data![index]);
+          },
+        );
+      },
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  final Vehicle vehicle;
+
+  CardWidget({required this.vehicle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.backgroundColor,
+      child: ListTile(),
+    );
+  }
+}
---
+++ fleetcarpooling/lib/pages/vehicle_managament_form.dart
@@ -0,0 +1,60 @@
+import 'package:fleetcarpooling/Modularity/pages/add_vehicle_form.dart';
+import 'package:fleetcarpooling/pages/delete_disable_form.dart';
+import 'package:fleetcarpooling/ui_elements/buttons.dart';
+import 'package:fleetcarpooling/ui_elements/colors';
+import 'package:flutter/material.dart';
+
+class VehicleManagamentForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        toolbarHeight: 80,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: AppColors.mainTextColor),
+        title: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(""),
+            Text("Vehicle managament",
+                style:
+                    TextStyle(color: AppColors.mainTextColor, fontSize: 25.0)),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 50),
+          MyElevatedButton(
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddVehicleForm()),
+                );
+              },
+              label: 'Add car manually od with QR'),
+          MyElevatedButton(
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DeleteDisableForm()),
+                );
+              },
+              label: 'Delete/disable car'),
+          Expanded(
+            child: Image.asset(
+              'assets/images/logo.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
