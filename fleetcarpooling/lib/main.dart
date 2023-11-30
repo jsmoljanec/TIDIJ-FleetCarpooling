38a528d24d9cc8b80b5ad5a4b63bc1265b86028d
Iva Plavšić <iplavsic20@student.foi.hr>
2023-11-30 13:40:52 +0100

    TIDIJ-28 Implemented so that when the notification is pressed, a new window opens with reservation details and car information.

--- fleetcarpooling/lib/main.dart
+++ fleetcarpooling/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:fleetcarpooling/pages/UserRegistrationForm.dart';
 import 'package:fleetcarpooling/pages/loginForm.dart';
-import 'package:fleetcarpooling/pages/navigation.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -9,7 +7,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(
+    options: const FirebaseOptions(
         apiKey: 'AIzaSyAiAzExpBKwIfaYhntOua3f7qNMJ5ecdA0',
         appId: '1:956285703635:android:1faaaa1cfb6be0d4d58b26',
         messagingSenderId: '956285703635',
