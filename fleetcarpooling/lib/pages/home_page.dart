f16978c79528baecd33e04121f62aec0b8920193
ijelovic20 <ijelovic20@foi.hr>
2023-12-04 11:36:30 +0100

    Implemented text for displaying selected date and profile icon for home page.

--- fleetcarpooling/lib/pages/home_page.dart
+++ fleetcarpooling/lib/pages/home_page.dart
@@ -1,9 +1,8 @@
-import 'package:fleetcarpooling/pages/profileForm.dart';
 import 'package:fleetcarpooling/ui_elements/colors';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -12,16 +11,83 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    double padding2 = screenHeight * 0.02;
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ProfilePage()),
-            );
-          },
-          child: Text("Click Me"),
+        child: Column(
+          children: [
+            Container(
+                width: screenWidth,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      top: padding2, bottom: padding2, left: 24, right: 24),
+                  child: Stack(
+                    fit: StackFit.loose,
+                    children: [
+                      const Align(
+                        alignment: Alignment.center,
+                        child: Row(
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  "Thu, 26 Oct",
+                                  style: TextStyle(
+                                      color: AppColors.mainTextColor,
+                                      fontSize: 24),
+                                ),
+                                Text(
+                                  "11:00",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w100,
+                                      color: AppColors.mainTextColor,
+                                      fontSize: 24),
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: AppColors.buttonColor,
+                                ),
+                                Text(
+                                  " ",
+                                  style: TextStyle(fontSize: 24),
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  "Fri, 27 Oct",
+                                  style: TextStyle(
+                                      color: AppColors.mainTextColor,
+                                      fontSize: 24),
+                                ),
+                                Text(
+                                  "17:30",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w100,
+                                      color: AppColors.mainTextColor,
+                                      fontSize: 24),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: SizedBox(
+                            child: Image.asset("icons/profile.png"),
+                          ))
+                    ],
+                  ),
+                ))
+          ],
         ),
       ),
     );
