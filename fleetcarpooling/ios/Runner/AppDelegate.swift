22344d4ff708140f577a2fa6c84ac90cb59ed57c
mtomasic2 <mtomasic20@student.foi.hr>
2023-12-05 23:45:29 +0100

    TIDIJ-72 Initial Google Maps setup

--- fleetcarpooling/ios/Runner/AppDelegate.swift
+++ fleetcarpooling/ios/Runner/AppDelegate.swift
@@ -8,6 +8,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyA4iJBiM6ZBt73O4bnomT03SDjDn_KPN30")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
