6f8930f62b69192d2f91bafdb4dbdb29226a143e
mtomasic2 <mtomasic20@student.foi.hr>
2023-12-07 18:47:33 +0100

    TIDIJ-72 Established connection with agent

--- fleetcarpooling/lib/pages/map.dart
+++ fleetcarpooling/lib/pages/map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:io';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -9,11 +10,21 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  late GoogleMapController mapController;
+  late RawDatagramSocket _clientSocket;
+  final destinationIPAddress = InternetAddress("10.0.2.2");
+  static const port = 50001;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  late GoogleMapController mapController;
+  final LatLng _center = const LatLng(46.303117, 16.324079);
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+  late String locationInfo = 'Waiting for location data...';
+  
+  Set<Marker> markers = {};
+
+  @override
+  void initState() {
+    connectUDP();
+    super.initState();
   }
 
   @override
@@ -24,18 +35,67 @@ class _MapPageState extends State<MapPage> {
         colorSchemeSeed: Colors.green[700],
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          elevation: 2,
-        ),
         body: GoogleMap(
-          onMapCreated: _onMapCreated,
+          onMapCreated: onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
             zoom: 11.0,
           ),
+          markers: markers,
         ),
       ),
     );
+  }
+
+  void onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  void connectUDP() async {
+    // print('Connecting to UDP server...');
+    _clientSocket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
+    // _clientSocket.send('Hello from Flutter'.codeUnits, destinationIPAddress, port);
+    sendCommand("current position");
+
+    _clientSocket.listen((RawSocketEvent event) {
+      if (event == RawSocketEvent.read) {
+        Datagram dg = _clientSocket.receive()!;
+        String message = String.fromCharCodes(dg.data);
+        RegExp regExp = RegExp(r"'latitude': (\d+\.\d+), 'longitude': (\d+\.\d+)");
+
+        var match = regExp.firstMatch(message);
+        if (match != null) {
+          var latitude = double.parse(match.group(1)!);
+          var longitude = double.parse(match.group(2)!);
+          print('$latitude, $longitude');
+
+          setState(() {
+            locationInfo = 'Latitude: $latitude, Longitude: $longitude';
+            markers = {
+              Marker(
+                markerId: const MarkerId('vehicle'),
+                position: LatLng(latitude, longitude),
+                infoWindow: const InfoWindow(title: 'Vehicle Location'),
+                icon: markerIcon,
+              ),
+            };
+          });
+        }
+      }
+    });
+  }
+
+  void sendCommand(String command) {
+    _clientSocket.send(command.codeUnits, destinationIPAddress, port);
+  }
+
+  void _dispose() {
+    _clientSocket.close();
+  }
+
+  @override
+  void dispose() {
+    _dispose();
+    super.dispose();
   }
 }
