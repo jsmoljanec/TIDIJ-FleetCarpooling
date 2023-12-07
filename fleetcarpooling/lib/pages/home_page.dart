de10d9a17ca6e05cfad8b8f2597b19804b186674
ijelovic20 <ijelovic20@foi.hr>
2023-12-07 19:46:32 +0100

    TIDIJ-14 Implemented displaying vehicle model names on screen. Added background photo.

--- fleetcarpooling/lib/pages/home_page.dart
+++ fleetcarpooling/lib/pages/home_page.dart
@@ -1,24 +1,28 @@
+import 'package:fleetcarpooling/Modularity/models/vehicle.dart';
+import 'package:fleetcarpooling/VehicleManagamentService/vehicle_managament_service.dart';
 import 'package:fleetcarpooling/ui_elements/colors';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  Stream<List<Vehicle>> vehicles = getVehicles();
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     double padding2 = screenHeight * 0.02;
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
                 width: screenWidth,
                 child: Padding(
                   padding: EdgeInsets.only(
@@ -80,57 +84,97 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                       Align(
-                          alignment: Alignment.centerRight,
-                          child: SizedBox(
-                            child: Image.asset("icons/profile.png"),
-                          )),
+                        alignment: Alignment.centerRight,
+                        child: SizedBox(
+                          child: Image.asset("icons/profile.png"),
+                        ),
+                      ),
                     ],
-                  ),
-                )),
-            Container(
-              width: screenWidth,
-              padding: const EdgeInsets.only(left: 24, right: 24),
-              child: SizedBox(
-                height: 43,
-                child: TextField(
-                  // textAlignVertical: TextAlignVertical.center,
-                  style: const TextStyle(
-                      color: AppColors.mainTextColor, fontSize: 16),
-                  // onChanged: ,
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(vertical: 12.0),
-                    filled: false,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(24),
-                      borderSide:
-                          const BorderSide(color: AppColors.mainTextColor),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(24),
-                      borderSide: const BorderSide(
-                        color: AppColors.buttonColor,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(24),
-                      borderSide: const BorderSide(
-                        color: AppColors.buttonColor,
-                      ),
-                    ),
-                    hintText: "Search..",
-                    hintStyle: const TextStyle(
-                      color: AppColors.unavailableColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w100,
-                      letterSpacing: 0.5,
-                    ),
-                    prefixIcon: const Icon(Icons.search),
-                    prefixIconColor: AppColors.mainTextColor,
                   ),
                 ),
               ),
-            )
-          ],
+              Container(
+                width: screenWidth,
+                padding: const EdgeInsets.only(left: 24, right: 24),
+                child: SizedBox(
+                  height: 43,
+                  child: TextField(
+                    style: const TextStyle(
+                        color: AppColors.mainTextColor, fontSize: 16),
+                    decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.symmetric(vertical: 12.0),
+                      filled: false,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        borderSide:
+                            const BorderSide(color: AppColors.mainTextColor),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        borderSide: const BorderSide(
+                          color: AppColors.buttonColor,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        borderSide: const BorderSide(
+                          color: AppColors.buttonColor,
+                        ),
+                      ),
+                      hintText: "Search..",
+                      hintStyle: const TextStyle(
+                        color: AppColors.unavailableColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w100,
+                        letterSpacing: 0.5,
+                      ),
+                      prefixIcon: const Icon(Icons.search),
+                      prefixIconColor: AppColors.mainTextColor,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                // decoration: const BoxDecoration(
+                //   image: DecorationImage(
+                //     image: AssetImage('images/logo.png'),
+                //     fit: BoxFit.cover,
+                //   ),
+                // ),
+                child: StreamBuilder<List<Vehicle>>(
+                  stream: getVehicles(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Center(
+                          child: Text('No vehicles available.'));
+                    }
+
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return Text(snapshot.data![index].model);
+                      },
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 400,
+                width: double.infinity,
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
