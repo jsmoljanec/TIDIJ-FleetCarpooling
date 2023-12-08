7e19d4fbc36529cb8c66e7e35b7531dd4c53149e
ijelovic20 <ijelovic20@foi.hr>
2023-12-08 23:34:54 +0100

    TIDIJ-14 Positioned background logo photo.

--- fleetcarpooling/lib/pages/home_page.dart
+++ fleetcarpooling/lib/pages/home_page.dart
@@ -18,204 +18,212 @@ class _HomePageState extends State<HomePage> {
     double screenWidth = MediaQuery.of(context).size.width;
     double padding2 = screenHeight * 0.02;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                width: screenWidth,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      top: padding2, bottom: padding2, left: 24, right: 24),
-                  child: Stack(
-                    fit: StackFit.loose,
-                    children: [
-                      const Align(
-                        alignment: Alignment.center,
-                        child: Row(
-                          children: [
-                            Column(
-                              children: [
-                                Text(
-                                  "Thu, 26 Oct",
-                                  style: TextStyle(
-                                      color: AppColors.mainTextColor,
-                                      fontSize: 24),
-                                ),
-                                Text(
-                                  "11:00",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w100,
-                                      color: AppColors.mainTextColor,
-                                      fontSize: 24),
-                                )
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Icon(
-                                  Icons.arrow_forward,
-                                  color: AppColors.buttonColor,
-                                ),
-                                Text(
-                                  " ",
-                                  style: TextStyle(fontSize: 24),
-                                )
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Text(
-                                  "Fri, 27 Oct",
-                                  style: TextStyle(
-                                      color: AppColors.mainTextColor,
-                                      fontSize: 24),
-                                ),
-                                Text(
-                                  "17:30",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w100,
-                                      color: AppColors.mainTextColor,
-                                      fontSize: 24),
-                                )
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: SizedBox(
-                          child: Image.asset("icons/profile.png"),
-                        ),
-                      ),
-                    ],
-                  ),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 250),
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/logo.png'),
+                  fit: BoxFit.cover,
                 ),
               ),
-              Container(
-                width: screenWidth,
-                padding: const EdgeInsets.only(left: 24, right: 24, bottom: 20),
-                child: SizedBox(
-                  height: 43,
-                  child: TextField(
-                    style: const TextStyle(
-                        color: AppColors.mainTextColor, fontSize: 16),
-                    decoration: InputDecoration(
-                      contentPadding:
-                          const EdgeInsets.symmetric(vertical: 12.0),
-                      filled: false,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(24),
-                        borderSide:
-                            const BorderSide(color: AppColors.mainTextColor),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(24),
-                        borderSide: const BorderSide(
-                          color: AppColors.buttonColor,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(24),
-                        borderSide: const BorderSide(
-                          color: AppColors.buttonColor,
-                        ),
-                      ),
-                      hintText: "Search..",
-                      hintStyle: const TextStyle(
-                        color: AppColors.unavailableColor,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w100,
-                        letterSpacing: 0.5,
-                      ),
-                      prefixIcon: const Icon(Icons.search),
-                      prefixIconColor: AppColors.mainTextColor,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('images/logo.png'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: StreamBuilder<List<Vehicle>>(
-                  stream: getVehicles(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-
-                    if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                      return const Center(
-                          child: Text('No vehicles available.'));
-                    }
-
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      physics: const BouncingScrollPhysics(),
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.fromLTRB(24, 0, 24, 12),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: AppColors.primaryColor,
-                              borderRadius: BorderRadius.circular(30.0),
-                              border: Border.all(
-                                color: AppColors.mainTextColor,
-                                width: 0.5,
-                              ),
-                            ),
-                            child: Column(
+            ),
+          ),
+          SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: screenWidth,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          top: padding2, bottom: padding2, left: 24, right: 24),
+                      child: Stack(
+                        fit: StackFit.loose,
+                        children: [
+                          const Align(
+                            alignment: Alignment.center,
+                            child: Row(
                               children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 12),
-                                  child: Image.network(
-                                    snapshot.data![index].imageUrl,
-                                    fit: BoxFit.cover,
-                                    height: 122,
-                                    width: 209,
-                                  ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      "Thu, 26 Oct",
+                                      style: TextStyle(
+                                          color: AppColors.mainTextColor,
+                                          fontSize: 24),
+                                    ),
+                                    Text(
+                                      "11:00",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w100,
+                                          color: AppColors.mainTextColor,
+                                          fontSize: 24),
+                                    )
+                                  ],
                                 ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(24, 20, 24, 20),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        snapshot.data![index].model,
-                                        style: const TextStyle(
-                                            color: AppColors.mainTextColor,
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 24),
-                                      ),
-                                      Text(
-                                        snapshot.data![index].transType,
-                                        style: const TextStyle(
-                                            color: AppColors.mainTextColor,
-                                            fontSize: 18),
-                                      )
-                                    ],
-                                  ),
+                                Column(
+                                  children: [
+                                    Icon(
+                                      Icons.arrow_forward,
+                                      color: AppColors.buttonColor,
+                                    ),
+                                    Text(
+                                      " ",
+                                      style: TextStyle(fontSize: 24),
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      "Fri, 27 Oct",
+                                      style: TextStyle(
+                                          color: AppColors.mainTextColor,
+                                          fontSize: 24),
+                                    ),
+                                    Text(
+                                      "17:30",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w100,
+                                          color: AppColors.mainTextColor,
+                                          fontSize: 24),
+                                    )
+                                  ],
                                 ),
                               ],
                             ),
                           ),
-                        );
-                      },
-                    );
-                  },
-                ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: SizedBox(
+                              child: Image.asset("assets/icons/profile.png"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: screenWidth,
+                    padding:
+                        const EdgeInsets.only(left: 24, right: 24, bottom: 20),
+                    child: SizedBox(
+                      height: 43,
+                      child: TextField(
+                        style: const TextStyle(
+                            color: AppColors.mainTextColor, fontSize: 16),
+                        decoration: InputDecoration(
+                          contentPadding:
+                              const EdgeInsets.symmetric(vertical: 12.0),
+                          filled: false,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                            borderSide: const BorderSide(
+                                color: AppColors.mainTextColor),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                            borderSide: const BorderSide(
+                              color: AppColors.buttonColor,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                            borderSide: const BorderSide(
+                              color: AppColors.buttonColor,
+                            ),
+                          ),
+                          hintText: "Search..",
+                          hintStyle: const TextStyle(
+                            color: AppColors.unavailableColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w100,
+                            letterSpacing: 0.5,
+                          ),
+                          prefixIcon: const Icon(Icons.search),
+                          prefixIconColor: AppColors.mainTextColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  StreamBuilder<List<Vehicle>>(
+                    stream: getVehicles(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return const Center(
+                            child: Text('No vehicles available.'));
+                      }
+
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        physics: const BouncingScrollPhysics(),
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.fromLTRB(24, 0, 24, 12),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: AppColors.primaryColor,
+                                borderRadius: BorderRadius.circular(30.0),
+                                border: Border.all(
+                                  color: AppColors.mainTextColor,
+                                  width: 0.5,
+                                ),
+                              ),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 12),
+                                    child: Image.network(
+                                      snapshot.data![index].imageUrl,
+                                      fit: BoxFit.cover,
+                                      height: 122,
+                                      width: 209,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        24, 20, 24, 20),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          snapshot.data![index].model,
+                                          style: const TextStyle(
+                                              color: AppColors.mainTextColor,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 24),
+                                        ),
+                                        Text(
+                                          snapshot.data![index].transType,
+                                          style: const TextStyle(
+                                              color: AppColors.mainTextColor,
+                                              fontSize: 18),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
