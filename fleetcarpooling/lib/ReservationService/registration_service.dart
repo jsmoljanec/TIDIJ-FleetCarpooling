56efac4dd228791e405555a09eeb5b3c97d171c7
Domagoj Šantek <dsantek20@foi.hr>
2023-12-12 19:31:32 +0100

    TIDIJ-19 Implemented function for retrieving reservations

---
+++ fleetcarpooling/lib/ReservationService/registration_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_database/firebase_database.dart';
+
+import '../Models/terms_model.dart';
+
+abstract class ReservationRepository {
+  Future<List<Terms>> getReservation(String vinCar);
+}
+
+class ReservationService implements ReservationRepository {
+  @override
+  Future<List<Terms>> getReservation(String vinCar) async {
+    List<Terms> termini = [];
+    final databaseReference = FirebaseDatabase.instance.ref();
+    var query = await databaseReference
+        .child("Reservation")
+        .orderByChild('VinCar')
+        .equalTo('556XYZ789LMN123OP');
+
+    DatabaseEvent snapshot = await query.once();
+    if (snapshot.snapshot.value != null) {
+      Map<dynamic, dynamic>? reservations =
+          snapshot.snapshot.value as Map<dynamic, dynamic>?;
+      reservations?.forEach((key, value) {
+        if (value['pickupDate'] != null) {
+          DateTime pickupDate =
+              DateTime.parse(value['pickupDate'] + ' ' + value['pickupTime']);
+          DateTime returnDate =
+              DateTime.parse(value['returnDate'] + ' ' + value['returnTime']);
+          termini.add(Terms(pickupDate, returnDate));
+        }
+      });
+    }
+    return termini;
+  }
+}
