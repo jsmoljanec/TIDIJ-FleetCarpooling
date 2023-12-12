d6539b256efc217c85f3896b22aefe0584d25f47
Domagoj Šantek <dsantek20@foi.hr>
2023-12-12 21:27:33 +0100

    TIDIJ-19 Extracted reserved terms

--- fleetcarpooling/lib/ReservationService/terms_service.dart
+++ fleetcarpooling/lib/ReservationService/terms_service.dart
@@ -1,3 +1,5 @@
+import '../Models/terms_model.dart';
+
 class TermsService {
   List<DateTime> createWorkHours(DateTime start, DateTime end) {
     List<DateTime> radniSati = [];
@@ -7,5 +9,23 @@ class TermsService {
       }
     }
     return radniSati;
+  }
+
+  List<DateTime> extractReservedTerms(List<Terms> terms) {
+    List<DateTime> reservedTerms = [];
+    for (Terms t in terms) {
+      for (int i = 0; i <= t.returnDate.difference(t.pickupDate).inDays; i++) {
+        for (int j = (i == 0 ? t.pickupDate.hour : 7);
+            j <=
+                (i < t.returnDate.difference(t.pickupDate).inDays
+                    ? 16
+                    : t.returnDate.hour);
+            j++) {
+          reservedTerms.add(DateTime(
+              t.pickupDate.year, t.pickupDate.month, t.pickupDate.day + i, j));
+        }
+      }
+    }
+    return reservedTerms;
   }
 }
