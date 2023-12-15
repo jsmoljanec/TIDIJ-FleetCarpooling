d9c1e26feb75305c2736593245665c564d6e52c0
ijelovic20 <ijelovic20@foi.hr>
2023-12-15 02:52:18 +0100

    TIDIJ-5 Implemented UI for calendar.

--- fleetcarpooling/lib/ui_elements/calendar.dart
+++ fleetcarpooling/lib/ui_elements/calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'colors' as my_defined_colors;
 
@@ -24,21 +25,52 @@ class MyCalendar extends StatelessWidget {
                 ),
                 defaultTextStyle:
                     TextStyle(color: my_defined_colors.AppColors.activeDays)),
-            headerStyle: const HeaderStyle(
+            headerStyle: HeaderStyle(
+              titleCentered: true,
               formatButtonVisible: false,
-              titleTextStyle: TextStyle(
+              titleTextStyle: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                   color: my_defined_colors.AppColors.mainTextColor),
+              leftChevronIcon: const Icon(Icons.arrow_back_ios_new_rounded,
+                  color: my_defined_colors.AppColors.mainTextColor),
+              rightChevronIcon: Transform.rotate(
+                angle: 3.141592,
+                child: const Icon(Icons.arrow_back_ios_new_rounded,
+                    color: my_defined_colors.AppColors.mainTextColor),
+              ),
             ),
-            daysOfWeekStyle: const DaysOfWeekStyle(
+            daysOfWeekStyle: DaysOfWeekStyle(
+              dowTextFormatter: (day, locale) {
+                final weekdayFormat = DateFormat('E', locale);
+                final formattedDay = weekdayFormat.format(day);
+                switch (formattedDay) {
+                  case 'Mon':
+                    return 'M';
+                  case 'Tue':
+                    return 'T';
+                  case 'Wed':
+                    return 'W';
+                  case 'Thu':
+                    return 'T';
+                  case 'Fri':
+                    return 'F';
+                  case 'Sat':
+                    return 'S';
+                  case 'Sun':
+                    return 'S';
+                  default:
+                    return formattedDay;
+                }
+              },
               weekdayStyle:
                   TextStyle(color: my_defined_colors.AppColors.activeDays),
               weekendStyle:
                   TextStyle(color: my_defined_colors.AppColors.activeDays),
             ),
             enabledDayPredicate: (day) {
-              return day.isAfter(DateTime.now().subtract(Duration(days: 1)));
+              return day
+                  .isAfter(DateTime.now().subtract(const Duration(days: 1)));
             },
           )),
     );
