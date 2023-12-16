e5e0511d58f8d80c95b0dd12860d2eac6facfab9
Domagoj Šantek <dsantek20@foi.hr>
2023-12-16 15:03:55 +0100

    TIDIJ-20 Display vehicle terms

--- fleetcarpooling/lib/pages/selected_vehicle_page.dart
+++ fleetcarpooling/lib/pages/selected_vehicle_page.dart
@@ -1,16 +1,48 @@
+import 'package:fleetcarpooling/Models/terms_model.dart';
+import 'package:fleetcarpooling/ReservationService/registration_service.dart';
+import 'package:fleetcarpooling/ReservationService/terms_service.dart';
 import 'package:fleetcarpooling/ui_elements/buttons.dart';
 import 'package:fleetcarpooling/ui_elements/calendar.dart';
 import 'package:flutter/material.dart';
 
 class SelectedVehiclePage extends StatefulWidget {
   final String vin;
-  const SelectedVehiclePage({super.key, required this.vin});
+
+  const SelectedVehiclePage({Key? key, required this.vin}) : super(key: key);
 
   @override
   State<SelectedVehiclePage> createState() => _SelectedVehiclePageState();
 }
 
 class _SelectedVehiclePageState extends State<SelectedVehiclePage> {
+  late ReservationService _service = ReservationService();
+  final TermsService _termsService = TermsService();
+  late List<Terms> termini;
+  late List<DateTime> busyTerms;
+  late List<DateTime> freeTerms;
+
+  @override
+  void initState() {
+    super.initState();
+    _service = ReservationService();
+    termini = [];
+    busyTerms = [];
+    freeTerms = [];
+
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    List<DateTime> radniSati = _termsService.createWorkHours(
+        DateTime.now(), DateTime.now().add(Duration(days: 365)));
+    termini = await _service.getReservation(widget.vin);
+    busyTerms = _termsService.extractReservedTerms(termini);
+    freeTerms =
+        radniSati.where((termin) => !busyTerms.contains(termin)).toList();
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,15 +62,18 @@ class _SelectedVehiclePageState extends State<SelectedVehiclePage> {
                 Align(
                   alignment: Alignment.centerRight,
                   child: InkWell(
-                      onTap: () {},
-                      child: Image.asset("assets/icons/chat.png")),
+                    onTap: () {},
+                    child: Image.asset("assets/icons/chat.png"),
+                  ),
                 ),
               ],
             ),
           ),
-          const MyCalendar(
+          MyCalendar(
             height: 200,
-            width: 220,
+            width: 300,
+            busyTerms: busyTerms,
+            freeTerms: freeTerms,
           ),
         ],
       ),
