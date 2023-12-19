13b77ae9e03af94429ae4adad51b4d696b26ace9
ijelovic20 <ijelovic20@foi.hr>
2023-12-19 02:19:23 +0100

    TIDIJ-5 Implemented storing data in database for reservation.

--- fleetcarpooling/lib/ReservationService/reservation_service.dart
+++ fleetcarpooling/lib/ReservationService/reservation_service.dart
@@ -1,1 +1,30 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
 
+class ReservationService {
+  Future<void> addReservation(String vin, String email, DateTime pickupDate,
+      DateTime returnDate, TimeOfDay pickupTime, TimeOfDay returnTime) async {
+    final DatabaseReference database = FirebaseDatabase.instance.reference();
+    DatabaseReference reservationRef = database.child("Reservation");
+    DatabaseReference newReservationRef = reservationRef.child(vin);
+
+    try {
+      await newReservationRef.set({
+        'vin': vin,
+        'email': email,
+        'pickupDate': pickupDate.toIso8601String(),
+        'returnDate': returnDate.toIso8601String(),
+        'pickupTime': _timeOfDayToMap(pickupTime),
+        'returnTime': _timeOfDayToMap(returnTime),
+      });
+      print('Reservation added successfully!');
+    } catch (error) {
+      print("Error adding reservation: $error");
+      throw error;
+    }
+  }
+
+  Map<String, dynamic> _timeOfDayToMap(TimeOfDay timeOfDay) {
+    return {'hour': timeOfDay.hour, 'minute': timeOfDay.minute};
+  }
+}
