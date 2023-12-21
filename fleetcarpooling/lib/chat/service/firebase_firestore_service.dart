ff52cc9486bd1b1838f3228dbefa8b7c274704d6
Domagoj Šantek <dsantek20@foi.hr>
2023-12-21 15:39:14 +0100

    TIDIJ-50 Implemented sending text messages

---
+++ fleetcarpooling/lib/chat/service/firebase_firestore_service.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fleetcarpooling/chat/models/message.dart';
+
+class FirebaseFirestoreService {
+  static final firestore = FirebaseFirestore.instance;
+
+  static Future<void> addTextMessage({
+    required String content,
+    required String receiverId,
+  }) async {
+    final message = Message(
+      content: content,
+      sentTime: DateTime.now(),
+      receiverId: receiverId,
+      messageType: MessageType.text,
+      senderId: FirebaseAuth.instance.currentUser!.uid,
+    );
+    await _addMessageToChat(receiverId, message);
+  }
+
+  static Future<void> _addMessageToChat(
+    String receiverId,
+    Message message,
+  ) async {
+    await firestore
+        .collection('chat')
+        .doc(receiverId)
+        .collection('message')
+        .add(message.toJson());
+  }
+}
