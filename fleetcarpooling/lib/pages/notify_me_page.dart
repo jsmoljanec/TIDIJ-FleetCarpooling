010b9fc25be24ab732c0820f6920917f7e9d7ea3
Iva Plavšić <iplavsic20@student.foi.hr>
2023-12-22 01:40:43 +0100

    TIDIJ-59 Implement navigation to the NotifyMe screen when the Notify me button is clicked within the SelectedVehiclePage, and return to the SelectedVehiclePage when tapping anywhere on the NotifyMe screen.

--- fleetcarpooling/lib/pages/notify_me_page.dart
+++ fleetcarpooling/lib/pages/notify_me_page.dart
@@ -2,43 +2,49 @@ import 'package:fleetcarpooling/ui_elements/colors';
 import 'package:flutter/material.dart';
 
 class NotifyMe extends StatelessWidget {
-  const NotifyMe({Key? key});
+  // ignore: use_key_in_widget_constructors
+  const NotifyMe();
 
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Container(
-        color: AppColors.backgroundColor,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Positioned(
-              top: screenHeight * 0.2,
-              child: Container(
-                height: 105,
-                width: 129,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/icons/check_mark.png'),
-                    fit: BoxFit.contain,
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Container(
+          color: AppColors.backgroundColor,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: screenHeight * 0.2,
+                child: Container(
+                  height: 105,
+                  width: 129,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icons/check_mark.png'),
+                      fit: BoxFit.contain,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const Center(
-              child: Text(
-                'YOU TURNED NOTIFICATIONS ON! WE WILL NOTIFY YOU AS SOON AS CAR IS AVAILABLE FOR YOUR DATE!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: AppColors.mainTextColor,
-                  fontSize: 36.0,
-                  fontWeight: FontWeight.bold,
+              const Center(
+                child: Text(
+                  'YOU TURNED NOTIFICATIONS ON! WE WILL NOTIFY YOU AS SOON AS CAR IS AVAILABLE FOR YOUR DATE!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: AppColors.mainTextColor,
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
