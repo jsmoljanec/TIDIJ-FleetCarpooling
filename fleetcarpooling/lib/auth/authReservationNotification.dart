3220d2fc8c865361f772349311f32b9de4b4a6cc
Iva Plavšić <iplavsic20@student.foi.hr>
2023-12-25 00:54:07 +0100

    TIDIJ-60 Implementing the function to save a reservation within the Notifications table when reserving a vehicle.

--- fleetcarpooling/lib/auth/authReservationNotification.dart
+++ fleetcarpooling/lib/auth/authReservationNotification.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:intl/intl.dart';
 
 class AuthReservationNotification {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -16,57 +17,45 @@ class AuthReservationNotification {
     return _auth.currentUser;
   }
 
-  AuthReservationNotification() {
-    _subscribeToReservationChanges();
-  }
-
-  void _subscribeToReservationChanges() {
-    _databaseReference.child('Reservation').onValue.listen((event) {
-      if (event.snapshot.value != null) {
-        Map<dynamic, dynamic>? reservations =
-            event.snapshot.value as Map<dynamic, dynamic>?;
-
-        if (reservations != null) {
-          DateTime now = DateTime.now();
-          User? currentUser = getCurrentUser();
-
-          reservations.forEach((key, value) {
-            if (value['email'] == currentUser?.email) {
-              if (value['pickupDate'] != null) {
-                DateTime pickupDate = DateTime.parse(value['pickupDate']);
-                DateTime pickupDateTime = DateTime.parse(
-                    '${value['pickupDate']} ${value['pickupTime']}');
-                if (pickupDate.isAfter(now) &&
-                    pickupDate.difference(now).inDays <= 2) {
-                  if (!(pickupDate.isAtSameMomentAs(now) &&
-                      pickupDateTime.isBefore(now))) {
-                    _saveNotificationToDatabase(
-                      'You have a reservation for ${value['pickupDate']} from ${value['pickupTime']} until ${value['returnDate']} ${value['returnTime']}.',
-                      value['vinCar']
-                    );
-                  }
-                }
-              }
-            }
-          });
-        }
-      }
-    });
-  }
-
-  Future<void> _saveNotificationToDatabase(String message, String VinCar) async {
+  Future<void> saveNotificationToDatabase(
+      Map<String, dynamic> reservationData) async {
     try {
       User? currentUser = getCurrentUser();
 
       if (currentUser != null) {
+        String message = _constructNotificationMessage(reservationData);
+
         await _databaseReference.child('Notifications').push().set({
           'userEmail': currentUser.email,
           'message': message,
-          'VinCar': VinCar,
+          'VinCar': reservationData['vinCar'],
+          'pickupDate': _formatDate(reservationData['pickupDate']),
+          'pickupTime': _formatTime(reservationData['pickupTime']),
+          'returnDate': _formatDate(reservationData['returnDate']),
+          'returnTime': _formatTime(reservationData['returnTime']),
         });
       }
     } catch (e) {
       throw Exception('Error saving notification to database');
     }
+  }
+
+  String _constructNotificationMessage(Map<String, dynamic> reservationData) {
+    String formattedPickupDate = _formatDate(reservationData['pickupDate']);
+    String formattedReturnDate = _formatDate(reservationData['returnDate']);
+    String formattedPickupTime = _formatTime(reservationData['pickupTime']);
+    String formattedReturnTime = _formatTime(reservationData['returnTime']);
+
+    return 'You have a reservation for $formattedPickupDate from $formattedPickupTime until $formattedReturnDate $formattedReturnTime.';
+  }
+
+  String _formatDate(String date) {
+    DateTime parsedDate = DateTime.parse(date);
+    return DateFormat('yyyy-MM-dd').format(parsedDate);
+  }
+
+  String _formatTime(String time) {
+    DateTime parsedTime = DateFormat('yyyy-MM-dd HH:mm').parse(time);
+    return DateFormat('HH:mm').format(parsedTime);
   }
 }
