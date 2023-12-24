6ad76f23c897264ce99eb56d7be94a6e9e97e114
Iva Plavšić <iplavsic20@student.foi.hr>
2023-12-24 21:47:14 +0100

    TIDIJ-185 The notification display logic has been changed to showcase notifications from the notification table. The data storage logic for reservations has been modified within the Notification table, and the data storage from the NotifyMe table to the Notification table has been fixed.

---
+++ fleetcarpooling/lib/auth/authReservationNotification.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class AuthReservationNotification {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final DatabaseReference _databaseReference = FirebaseDatabase.instance.ref();
+  final StreamController<List<Map<String, dynamic>>>
+      _reservationStreamController =
+      StreamController<List<Map<String, dynamic>>>.broadcast();
+
+  Stream<List<Map<String, dynamic>>> get reservationStream =>
+      _reservationStreamController.stream;
+
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
+
+  AuthReservationNotification() {
+    _subscribeToReservationChanges();
+  }
+
+  void _subscribeToReservationChanges() {
+    _databaseReference.child('Reservation').onValue.listen((event) {
+      if (event.snapshot.value != null) {
+        Map<dynamic, dynamic>? reservations =
+            event.snapshot.value as Map<dynamic, dynamic>?;
+
+        if (reservations != null) {
+          DateTime now = DateTime.now();
+          User? currentUser = getCurrentUser();
+
+          reservations.forEach((key, value) {
+            if (value['email'] == currentUser?.email) {
+              if (value['pickupDate'] != null) {
+                DateTime pickupDate = DateTime.parse(value['pickupDate']);
+                DateTime pickupDateTime = DateTime.parse(
+                    '${value['pickupDate']} ${value['pickupTime']}');
+                if (pickupDate.isAfter(now) &&
+                    pickupDate.difference(now).inDays <= 2) {
+                  if (!(pickupDate.isAtSameMomentAs(now) &&
+                      pickupDateTime.isBefore(now))) {
+                    _saveNotificationToDatabase(
+                      'You have a reservation for ${value['pickupDate']} from ${value['pickupTime']} until ${value['returnDate']} ${value['returnTime']}.',
+                      value['vinCar'],
+                    );
+                  }
+                }
+              }
+            }
+          });
+        }
+      }
+    });
+  }
+
+  Future<void> _saveNotificationToDatabase(String message, String VinCar) async {
+    try {
+      User? currentUser = getCurrentUser();
+
+      if (currentUser != null) {
+        await _databaseReference.child('Notifications').push().set({
+          'userEmail': currentUser.email,
+          'message': message,
+          'VinCar': VinCar,
+        });
+      }
+    } catch (e) {
+      throw Exception('Error saving notification to database');
+    }
+  }
+}
