7ace1936d65afa95df6fdaf7888f3459590b5dc2
ijelovic20 <ijelovic20@foi.hr>
2023-12-28 18:59:42 +0100

    TIDIJ-164 Implemented top of the screen and added logo photo.

--- fleetcarpooling/lib/pages/my_reservations.dart
+++ fleetcarpooling/lib/pages/my_reservations.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fleetcarpooling/ui_elements/colors';
 
 class MyReservationsPage extends StatefulWidget {
   const MyReservationsPage({super.key});
@@ -8,11 +9,65 @@ class MyReservationsPage extends StatefulWidget {
 }
 
 class _MyReservationsPageState extends State<MyReservationsPage> {
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("My reservations page"),
-    );
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    double padding2 = screenHeight * 0.02;
+
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.only(top: 25.0),
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 450),
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/logo.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 50,
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(color: AppColors.buttonColor))),
+            child: const Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                "MY RESERVATIONS",
+                style: TextStyle(color: AppColors.mainTextColor, fontSize: 24),
+              ),
+            ),
+          )
+        ],
+      ),
+    ));
+
+    // return Scaffold(
+    //   body: Column(
+    //     children: [
+    //       Container(
+    //         margin: const EdgeInsets.only(bottom: 27.5),
+    //         width: screenWidth,
+    //         decoration: const BoxDecoration(
+    //           border: Border(bottom: BorderSide(color: AppColors.buttonColor)),
+    //         ),
+    //         child: Padding(
+    //           padding: EdgeInsets.only(top: padding2, bottom: padding2),
+    //           child: const Text(
+    //             "MY RESERVATIONS",
+    //             textAlign: TextAlign.center,
+    //             style: TextStyle(color: AppColors.mainTextColor, fontSize: 24),
+    //           ),
+    //         ),
+    //       )
+    //     ],
+    //   ),
+    // );
   }
 }
