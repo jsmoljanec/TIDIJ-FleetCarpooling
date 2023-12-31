fb0fc43029401638a6b1e0ff18d693cea3e359ff
Domagoj Šantek <dsantek20@foi.hr>
2023-12-31 09:26:43 +0100

    TIDIJ-206 Fixed render error

--- fleetcarpooling/lib/pages/selected_vehicle_page.dart
+++ fleetcarpooling/lib/pages/selected_vehicle_page.dart
@@ -1,3 +1,5 @@
+import 'package:fleetcarpooling/ui_elements/colors';
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fleetcarpooling/ReservationService/reservation_service.dart';
 import 'package:fleetcarpooling/Modularity/models/vehicle.dart';
@@ -8,8 +10,6 @@ import 'package:fleetcarpooling/chat/pages/chat_screen.dart';
 import 'package:fleetcarpooling/pages/reservation_form.dart';
 import 'package:fleetcarpooling/ui_elements/buttons.dart';
 import 'package:fleetcarpooling/ui_elements/calendar.dart';
-import 'package:fleetcarpooling/ui_elements/colors';
-import 'package:flutter/material.dart';
 
 class SelectedVehiclePage extends StatefulWidget {
   final String vin;
@@ -17,13 +17,13 @@ class SelectedVehiclePage extends StatefulWidget {
   final DateTime pickupTime;
   final DateTime returnTime;
 
-  const SelectedVehiclePage(
-      {Key? key,
-      required this.vin,
-      required this.isFree,
-      required this.pickupTime,
-      required this.returnTime})
-      : super(key: key);
+  const SelectedVehiclePage({
+    Key? key,
+    required this.vin,
+    required this.isFree,
+    required this.pickupTime,
+    required this.returnTime,
+  }) : super(key: key);
 
   @override
   State<SelectedVehiclePage> createState() => _SelectedVehiclePageState();
@@ -38,16 +38,22 @@ class _SelectedVehiclePageState extends State<SelectedVehiclePage> {
     String email = FirebaseAuth.instance.currentUser!.email!;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            // ignore: sort_child_properties_last
-            child: StreamBuilder<Vehicle?>(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            StreamBuilder<Vehicle?>(
               stream: getVehicle(widget.vin),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
+                  return Center(child: CircularProgressIndicator());
                 }
+
+                if (!snapshot.hasData) {
+                  return Center(child: Text('No data available'));
+                }
+
+                Vehicle vehicle = snapshot.data!;
+
                 return Column(
                   children: [
                     Padding(
@@ -60,13 +66,13 @@ class _SelectedVehiclePageState extends State<SelectedVehiclePage> {
                             },
                           ),
                           Text(
-                            "${snapshot.data!.brand} ${snapshot.data!.model}",
+                            "${vehicle.brand} ${vehicle.model}",
                             style: const TextStyle(
                               fontSize: 24,
                               color: AppColors.mainTextColor,
                             ),
                           ),
-                          const Spacer(),
+                          Spacer(),
                           Align(
                             alignment: Alignment.centerRight,
                             child: InkWell(
@@ -74,11 +80,12 @@ class _SelectedVehiclePageState extends State<SelectedVehiclePage> {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => ChatScreen(
-                                            vin: snapshot.data!.vin,
-                                            brand: snapshot.data!.brand,
-                                            model: snapshot.data!.model,
-                                          )),
+                                    builder: (context) => ChatScreen(
+                                      vin: vehicle.vin,
+                                      brand: vehicle.brand,
+                                      model: vehicle.model,
+                                    ),
+                                  ),
                                 );
                               },
                               child: Image.asset("assets/icons/chat.png"),
@@ -88,83 +95,80 @@ class _SelectedVehiclePageState extends State<SelectedVehiclePage> {
                       ),
                     ),
                     Image.network(
-                      snapshot.data!.imageUrl,
+                      vehicle.imageUrl,
                       fit: BoxFit.cover,
                       height: 150,
                       width: 300,
                     ),
-                    Expanded(
-                      child: Container(
-                        height: double.infinity,
-                        decoration: const BoxDecoration(
-                          color: AppColors.primaryColor,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30),
-                            topRight: Radius.circular(30),
-                          ),
-                          border: Border(
-                            top: BorderSide(color: AppColors.mainTextColor),
-                            left: BorderSide(color: AppColors.mainTextColor),
-                            right: BorderSide(color: AppColors.mainTextColor),
-                          ),
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: AppColors.primaryColor,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
                         ),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.fromLTRB(32, 28, 32, 20),
-                              child: Row(
-                                children: [
-                                  SizedBox(
-                                    height: 20,
-                                    width: 20,
-                                    child: Image.asset("assets/icons/fuel.png"),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 8.0, top: 4),
-                                    child: Text(
-                                      "${snapshot.data!.fuelConsumption} l/100km",
-                                      style: const TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w700,
-                                          color: AppColors.mainTextColor),
+                        border: Border(
+                          top: BorderSide(color: AppColors.mainTextColor),
+                          left: BorderSide(color: AppColors.mainTextColor),
+                          right: BorderSide(color: AppColors.mainTextColor),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(32, 28, 32, 20),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  height: 20,
+                                  width: 20,
+                                  child: Image.asset("assets/icons/fuel.png"),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(left: 8.0, top: 4),
+                                  child: Text(
+                                    "${vehicle.fuelConsumption} l/100km",
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700,
+                                      color: AppColors.mainTextColor,
                                     ),
                                   ),
-                                  const Spacer(),
-                                  SizedBox(
-                                      width: 20,
-                                      height: 20,
-                                      child: Image.asset(
-                                          "assets/icons/distance.png")),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 8.0, top: 4),
-                                    child: Text(
-                                      "${snapshot.data!.distanceTraveled} km",
-                                      style: const TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w700,
-                                          color: AppColors.mainTextColor),
+                                ),
+                                Spacer(),
+                                SizedBox(
+                                  width: 20,
+                                  height: 20,
+                                  child:
+                                      Image.asset("assets/icons/distance.png"),
+                                ),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(left: 8.0, top: 4),
+                                  child: Text(
+                                    "${vehicle.distanceTraveled} km",
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700,
+                                      color: AppColors.mainTextColor,
                                     ),
-                                  )
-                                ],
-                              ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 );
               },
             ),
-            flex: 3,
-          ),
-          Expanded(
-            flex: 4,
-            child: Container(
-              decoration: const BoxDecoration(
+            Container(
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
                 color: AppColors.primaryColor,
                 borderRadius: BorderRadius.only(),
                 border: Border(
@@ -172,16 +176,25 @@ class _SelectedVehiclePageState extends State<SelectedVehiclePage> {
                   right: BorderSide(color: AppColors.mainTextColor),
                 ),
               ),
-              height: double.infinity,
+              child: MyCalendar(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  vin: widget.vin),
+            ),
+            Container(
               width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: [
-                  MyCalendar(
-                    height: double.infinity,
-                    width: MediaQuery.of(context).size.width,
-                    vin: widget.vin,
-                  ),
-                  InkWell(
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.only(),
+                border: Border(
+                  left: BorderSide(color: AppColors.mainTextColor),
+                  right: BorderSide(color: AppColors.mainTextColor),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Center(
+                  child: InkWell(
                     onTap: () {},
                     child: const Text(
                       "Change date",
@@ -193,47 +206,61 @@ class _SelectedVehiclePageState extends State<SelectedVehiclePage> {
                       ),
                     ),
                   ),
-                  MyElevatedButton(
-                    onPressed: () async {
-                      if (widget.isFree == true) {
-                        await service.addReservation(
-                          widget.vin,
-                          widget.pickupTime,
-                          widget.returnTime,
-                        );
-                        await service.confirmRegistration(
-                          email,
-                          widget.pickupTime,
-                          widget.returnTime,
-                        );
-                        await authReservationNotification
-                            .saveNotificationToDatabase({
-                          'vinCar': widget.vin,
-                          'pickupDate': widget.pickupTime.toLocal().toString(),
-                          'pickupTime': widget.pickupTime.toLocal().toString(),
-                          'returnDate': widget.returnTime.toLocal().toString(),
-                          'returnTime': widget.returnTime.toLocal().toString(),
-                        });
-                      } else {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => NotifyMe(
-                              vinCar: "snapshot.data!.vin",
-                              pickupDateTime: widget.pickupTime.toLocal(),
-                              returnDateTime: widget.returnTime.toLocal(),
-                            ),
-                          ),
-                        );
-                      }
-                    },
-                    label: widget.isFree ? "MAKE A RESERVATION" : "NOTIFY ME",
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-        ],
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.only(),
+                border: Border(
+                  left: BorderSide(color: AppColors.mainTextColor),
+                  right: BorderSide(color: AppColors.mainTextColor),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: MyElevatedButton(
+                  onPressed: () async {
+                    if (widget.isFree == true) {
+                      await service.addReservation(
+                        widget.vin,
+                        widget.pickupTime,
+                        widget.returnTime,
+                      );
+                      await service.confirmRegistration(
+                        email,
+                        widget.pickupTime,
+                        widget.returnTime,
+                      );
+                      await authReservationNotification
+                          .saveNotificationToDatabase({
+                        'vinCar': widget.vin,
+                        'pickupDate': widget.pickupTime.toLocal().toString(),
+                        'pickupTime': widget.pickupTime.toLocal().toString(),
+                        'returnDate': widget.returnTime.toLocal().toString(),
+                        'returnTime': widget.returnTime.toLocal().toString(),
+                      });
+                    } else {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => NotifyMe(
+                            vinCar: "snapshot.data!.vin",
+                            pickupDateTime: widget.pickupTime.toLocal(),
+                            returnDateTime: widget.returnTime.toLocal(),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  label: widget.isFree ? "MAKE A RESERVATION" : "NOTIFY ME",
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- fleetcarpooling/lib/ui_elements/calendar.dart
+++ fleetcarpooling/lib/ui_elements/calendar.dart
@@ -36,123 +36,126 @@ class MyCalendar extends StatelessWidget {
         freeTerms =
             workHours.where((termin) => !busyTerms.contains(termin)).toList();
 
-        return Expanded(
-          child: SizedBox(
-            width: width,
-            height: height,
-            child: TableCalendar(
-              rowHeight: 35,
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              focusedDay: initialFocusedDay,
-              onPageChanged: (focusedDay) {
-                DateTime newFocusedDay =
-                    DateTime(focusedDay.year, focusedDay.month, 1);
-                initialFocusedDay = newFocusedDay;
-                print('New focused day: $newFocusedDay');
-              },
-              calendarStyle: const CalendarStyle(
-                  outsideDaysVisible: false,
-                  todayDecoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: my_defined_colors.AppColors.primaryColor,
-                  ),
-                  todayTextStyle: TextStyle(
-                    color: my_defined_colors.AppColors.activeDays,
-                  ),
-                  weekendTextStyle: TextStyle(
+        return Column(
+          children: [
+            SizedBox(
+              width: width,
+              height: height,
+              child: TableCalendar(
+                rowHeight: 35,
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: initialFocusedDay,
+                onPageChanged: (focusedDay) {
+                  DateTime newFocusedDay =
+                      DateTime(focusedDay.year, focusedDay.month, 1);
+                  initialFocusedDay = newFocusedDay;
+                  print('New focused day: $newFocusedDay');
+                },
+                calendarStyle: const CalendarStyle(
+                    outsideDaysVisible: false,
+                    todayDecoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: my_defined_colors.AppColors.primaryColor,
+                    ),
+                    todayTextStyle: TextStyle(
+                      color: my_defined_colors.AppColors.activeDays,
+                    ),
+                    weekendTextStyle: TextStyle(
+                        color: my_defined_colors.AppColors.mainTextColor),
+                    disabledTextStyle: TextStyle(
+                      color: my_defined_colors.AppColors.unavailableColor,
+                    ),
+                    selectedTextStyle: TextStyle(
+                      color: my_defined_colors.AppColors.activeDays,
+                    ),
+                    defaultTextStyle: TextStyle(
+                        color: my_defined_colors.AppColors.activeDays)),
+                headerStyle: HeaderStyle(
+                  titleCentered: true,
+                  formatButtonVisible: false,
+                  titleTextStyle: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                       color: my_defined_colors.AppColors.mainTextColor),
-                  disabledTextStyle: TextStyle(
-                    color: my_defined_colors.AppColors.unavailableColor,
-                  ),
-                  selectedTextStyle: TextStyle(
-                    color: my_defined_colors.AppColors.activeDays,
-                  ),
-                  defaultTextStyle:
-                      TextStyle(color: my_defined_colors.AppColors.activeDays)),
-              headerStyle: HeaderStyle(
-                titleCentered: true,
-                formatButtonVisible: false,
-                titleTextStyle: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: my_defined_colors.AppColors.mainTextColor),
-                leftChevronIcon: const Icon(Icons.arrow_back_ios_new_rounded,
-                    color: my_defined_colors.AppColors.mainTextColor),
-                rightChevronIcon: Transform.rotate(
-                  angle: 3.141592,
-                  child: const Icon(Icons.arrow_back_ios_new_rounded,
+                  leftChevronIcon: const Icon(Icons.arrow_back_ios_new_rounded,
                       color: my_defined_colors.AppColors.mainTextColor),
+                  rightChevronIcon: Transform.rotate(
+                    angle: 3.141592,
+                    child: const Icon(Icons.arrow_back_ios_new_rounded,
+                        color: my_defined_colors.AppColors.mainTextColor),
+                  ),
                 ),
-              ),
-              daysOfWeekStyle: DaysOfWeekStyle(
-                dowTextFormatter: (day, locale) {
-                  final weekdayFormat = DateFormat('E', locale);
-                  final formattedDay = weekdayFormat.format(day);
-                  switch (formattedDay) {
-                    case 'Mon':
-                      return 'M';
-                    case 'Tue':
-                      return 'T';
-                    case 'Wed':
-                      return 'W';
-                    case 'Thu':
-                      return 'T';
-                    case 'Fri':
-                      return 'F';
-                    case 'Sat':
-                      return 'S';
-                    case 'Sun':
-                      return 'S';
-                    default:
-                      return formattedDay;
-                  }
+                daysOfWeekStyle: DaysOfWeekStyle(
+                  dowTextFormatter: (day, locale) {
+                    final weekdayFormat = DateFormat('E', locale);
+                    final formattedDay = weekdayFormat.format(day);
+                    switch (formattedDay) {
+                      case 'Mon':
+                        return 'M';
+                      case 'Tue':
+                        return 'T';
+                      case 'Wed':
+                        return 'W';
+                      case 'Thu':
+                        return 'T';
+                      case 'Fri':
+                        return 'F';
+                      case 'Sat':
+                        return 'S';
+                      case 'Sun':
+                        return 'S';
+                      default:
+                        return formattedDay;
+                    }
+                  },
+                  weekdayStyle: const TextStyle(
+                      color: my_defined_colors.AppColors.activeDays),
+                  weekendStyle: const TextStyle(
+                      color: my_defined_colors.AppColors.activeDays),
+                ),
+                enabledDayPredicate: (day) {
+                  return day.isAfter(
+                      DateTime.now().subtract(const Duration(days: 1)));
                 },
-                weekdayStyle: const TextStyle(
-                    color: my_defined_colors.AppColors.activeDays),
-                weekendStyle: const TextStyle(
-                    color: my_defined_colors.AppColors.activeDays),
-              ),
-              enabledDayPredicate: (day) {
-                return day
-                    .isAfter(DateTime.now().subtract(const Duration(days: 1)));
-              },
-              calendarBuilders: CalendarBuilders(
-                defaultBuilder: (context, day, focusedDay) {
-                  if (freeTerms.any((termin) => isSameDay(termin, day))) {
-                    return Container(
-                      child: Center(
-                        child: Text(
-                          '${day.day}',
-                          style: TextStyle().copyWith(
-                              color: my_defined_colors.AppColors.activeDays),
+                calendarBuilders: CalendarBuilders(
+                  defaultBuilder: (context, day, focusedDay) {
+                    if (freeTerms.any((termin) => isSameDay(termin, day))) {
+                      return Container(
+                        child: Center(
+                          child: Text(
+                            '${day.day}',
+                            style: TextStyle().copyWith(
+                                color: my_defined_colors.AppColors.activeDays),
+                          ),
                         ),
-                      ),
-                    );
-                  } else if (busyTerms
-                      .any((termin) => isSameDay(termin, day))) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: my_defined_colors.AppColors.reservedInCalendar,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Center(
-                        child: Text(
-                          '${day.day}',
-                          style: TextStyle().copyWith(
-                              color: my_defined_colors.AppColors.mainTextColor),
+                      );
+                    } else if (busyTerms
+                        .any((termin) => isSameDay(termin, day))) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: my_defined_colors.AppColors.reservedInCalendar,
+                          shape: BoxShape.circle,
                         ),
-                      ),
-                    );
-                  }
-                  return null;
+                        child: Center(
+                          child: Text(
+                            '${day.day}',
+                            style: TextStyle().copyWith(
+                                color:
+                                    my_defined_colors.AppColors.mainTextColor),
+                          ),
+                        ),
+                      );
+                    }
+                    return null;
+                  },
+                ),
+                onDaySelected: (selectedDay, focusedDay) {
+                  _showAvailableHoursAlert(context, selectedDay, freeTerms);
                 },
               ),
-              onDaySelected: (selectedDay, focusedDay) {
-                _showAvailableHoursAlert(context, selectedDay, freeTerms);
-              },
             ),
-          ),
+          ],
         );
       },
     );
