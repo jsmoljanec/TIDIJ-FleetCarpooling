4231cd48a264e155d4d43d08c723f607e7cddbb2
Domagoj Šantek <dsantek20@foi.hr>
2024-01-03 14:16:59 +0100

    TIDIJ-191 Added qr addition module

--- qraddition/lib/add_vehicle_QR_form.dart
+++ qraddition/lib/add_vehicle_QR_form.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
+import 'package:core/add_vehicle_interface.dart';
+import 'package:core/bloc/vehicle_bloc.dart';
 import 'package:core/event/vehicle_event.dart';
 import 'package:core/state/vehicle_state.dart';
 import 'package:core/vehicle.dart';
-import 'package:fleetcarpooling/Modularity/Pages/add_vehicle_interface.dart';
-import 'package:fleetcarpooling/Modularity/bloc/vehicle_bloc.dart';
-
 import 'package:fleetcarpooling/ui_elements/buttons.dart';
 import 'package:fleetcarpooling/ui_elements/colors';
 import 'package:flutter/material.dart';
