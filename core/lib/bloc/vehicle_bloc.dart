bce7c8bf6006d4610931232846a98329d5478f22
Domagoj Šantek <dsantek20@foi.hr>
2024-01-06 19:48:19 +0100

    TIDIJ-191 Added that each project can be started separately.

--- core/lib/bloc/vehicle_bloc.dart
+++ core/lib/bloc/vehicle_bloc.dart
@@ -4,8 +4,8 @@ import 'package:core/state/vehicle_state.dart';
 import 'package:fleetcarpooling/VehicleManagamentService/vehicle_managament_service.dart';
 
 class VehicleBloc extends Bloc<VehicleEvent, VehicleState> {
-  final VehicleRepository _vehicleRepository;
-  VehicleBloc(this._vehicleRepository) : super(VehicleInitialState()) {
+  final VehicleRepository _vehicleRepository = VehicleService();
+  VehicleBloc() : super(VehicleInitialState()) {
     on<AddVehicleEvent>((event, emit) async {
       emit(VehicleLoadingState());
 
