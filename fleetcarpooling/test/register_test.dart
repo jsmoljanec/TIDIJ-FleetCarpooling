d73f162d0be01a7c801254deb990f920fc54934d
Domagoj Šantek <dsantek20@foi.hr>
2024-01-27 22:53:27 +0100

    TIDIJ-228 Quick fix

--- fleetcarpooling/test/register_test.dart
+++ fleetcarpooling/test/register_test.dart
@@ -49,62 +49,63 @@ void main() {
     authRegistrationService = AuthRegistrationService(firebaseDatabase, auth);
     dotenv.load();
   });
+  group("UserRegistration", () {
+    test('Generates a random password of correct length', () {
+      final password = generateRandomPassword();
+      expect(password.length, equals(8));
+    });
 
-  test('Generates a random password of correct length', () {
-    final password = generateRandomPassword();
-    expect(password.length, equals(8));
-  });
+    test('Generates a random password of correct characters', () {
+      const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+      final password = generateRandomPassword();
+      for (int i = 0; i < password.length; i++) {
+        expect(chars.contains(password[i]), isTrue);
+      }
+    });
+    test('Should write user in database', () async {
+      // Define new user data
+      const newUserId = 'newUserId';
+      const newUserName = 'New User';
+      const newUserEmail = 'new.user@email.com';
+      const newUserFirstName = 'New';
+      const newUserLastName = 'User';
+      const newUserRole = 'User';
+      const newUser = {
+        'username': newUserName,
+        'email': newUserEmail,
+        'firstName': newUserFirstName,
+        'lastName': newUserLastName,
+        'role': newUserRole,
+        'profileImage': '',
+        'statusActivity': 'offline',
+      };
 
-  test('Generates a random password of correct characters', () {
-    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    final password = generateRandomPassword();
-    for (int i = 0; i < password.length; i++) {
-      expect(chars.contains(password[i]), isTrue);
-    }
-  });
-  test('Should write user in database', () async {
-    // Define new user data
-    const newUserId = 'newUserId';
-    const newUserName = 'New User';
-    const newUserEmail = 'new.user@email.com';
-    const newUserFirstName = 'New';
-    const newUserLastName = 'User';
-    const newUserRole = 'User';
-    const newUser = {
-      'username': newUserName,
-      'email': newUserEmail,
-      'firstName': newUserFirstName,
-      'lastName': newUserLastName,
-      'role': newUserRole,
-      'profileImage': '',
-      'statusActivity': 'offline',
-    };
+      authRegistrationService?.writeDataToDatabase(newUserId, newUserName,
+          newUserEmail, newUserFirstName, newUserLastName, newUserRole);
 
-    authRegistrationService?.writeDataToDatabase(newUserId, newUserName,
-        newUserEmail, newUserFirstName, newUserLastName, newUserRole);
+      final newUserFromDatabase = await userRepository?.getUser(newUserId);
 
-    final newUserFromDatabase = await userRepository?.getUser(newUserId);
+      expect(
+        newUserFromDatabase,
+        equals(newUser),
+      );
+    });
+    test('Sending email should pass with valid inputs', () async {
+      const email = 'domagojsantek70@gmail.com';
+      const usernameLogin = 'testUser';
+      const passwordLogin = 'testPassword';
+      expect(sendEmail(email, auth, usernameLogin, passwordLogin), completes);
+    });
 
-    expect(
-      newUserFromDatabase,
-      equals(newUser),
-    );
-  });
-  test('Sending email should pass with valid inputs', () async {
-    const email = 'domagojsantek70@gmail.com';
-    const usernameLogin = 'testUser';
-    const passwordLogin = 'testPassword';
-    expect(sendEmail(email, auth, usernameLogin, passwordLogin), completes);
-  });
+    test('Should throws MailerException', () async {
+      const email = 'mail'; // Invalid email
+      const usernameLogin = 'testUser';
+      const passwordLogin = 'testPassword';
 
-  test('Should throws MailerException', () async {
-    const email = 'mail'; // Invalid email
-    const usernameLogin = 'testUser';
-    const passwordLogin = 'testPassword';
-
-    await expectLater(
-      sendEmail(email, auth, usernameLogin, passwordLogin),
-      throwsA(isA<MailerException>()),
-    );
+      await expectLater(
+        sendEmail(email, auth, usernameLogin, passwordLogin),
+        throwsA(isA<MailerException>()),
+      );
+    });
   });
 }
